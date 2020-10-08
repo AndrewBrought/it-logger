3751aa75f4492bc152eb320c6d4d745d66a4ec74
Andrew Brought <andrewbrought@gmail.com>
2020-10-08 17:22:14 -0500

    added a note

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,9 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 // In here is where we'll have any actions
 //The first we want to have is to fetch the logs
 
+
+// ** THIS IS THE SAME AS THE CODE IN ACTION BELOW
+
 // export const getLogs = () => {
 //     return async (dispatch) => {
 //         setLoading();

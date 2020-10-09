9e88048fccb7986504e65df8706bd659195ab9ff
Andrew Brought <andrewbrought@gmail.com>
2020-10-09 12:44:22 -0500

    completed update logs, starting search logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from '../actions/types';
+import {
+    GET_LOGS,
+    SET_LOADING,
+    LOGS_ERROR,
+    ADD_LOG,
+    DELETE_LOG,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_LOG
+} from '../actions/types';
 
 const initialState = {
     logs: null,
@@ -14,6 +23,16 @@ export default (state = initialState, action) => {
                 ...state,
                 logs: action.payload,
                 loading: false
+            }
+        case SET_CURRENT:
+            return {
+                ...state,
+                current: action.payload
+            }
+        case CLEAR_CURRENT:
+            return {
+                ...state,
+                current: null
             }
         case SET_LOADING:
             return {
@@ -34,6 +53,13 @@ export default (state = initialState, action) => {
                 ...state,
                 logs: state.logs.filter(log => log.id !== action.payload),
                 loading: false
+            }
+        case UPDATE_LOG:
+            return {
+                ...state,
+                // We're mapping through the logs and comparing each log to the action.payload, if match,
+                // then we're returning the updated log (action.payload) else return the log in state
+                logs: state.logs.map(log => log.id === action.payload.id ? action.payload : log)
             }
         case LOGS_ERROR:
             console.error(action.payload)
